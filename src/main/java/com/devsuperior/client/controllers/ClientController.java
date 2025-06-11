26dd54576d3dcc0533fc065570f29a98cbb521d4
Fábio José <fabiojfl@gmail.com>
2025-06-11 13:51:23 -0300

    Criado uma classe ClientController para teste de endpoint

---
+++ src/main/java/com/devsuperior/client/controllers/ClientController.java
@@ -0,0 +1,14 @@
+package com.devsuperior.client.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="clients")
+public class ClientController {
+    @GetMapping
+    public String teste(){
+        return "Olá Mundo =)";
+    }
+}
