2c4301802029b31169809c1c15ef470a19d55f68
Fábio José <fabiojfl@gmail.com>
2025-06-13 06:10:12 -0300

    GET Client by Id

--- src/main/java/com/devsuperior/client/controllers/ClientController.java
+++ src/main/java/com/devsuperior/client/controllers/ClientController.java
@@ -1,14 +1,22 @@
 package com.devsuperior.client.controllers;
 
+import com.devsuperior.client.dto.ClientDTO;
+import com.devsuperior.client.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="clients")
+@RequestMapping(value="/clients")
 public class ClientController {
-    @GetMapping
-    public String teste(){
-        return "Olá Mundo =)";
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value="/{id}")
+    public ClientDTO findById(@PathVariable Long id){
+        return service.findById(id);
     }
 }
---
+++ src/main/java/com/devsuperior/client/services/ClientService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.client.services;
+
+import com.devsuperior.client.dto.ClientDTO;
+import com.devsuperior.client.entities.Client;
+import com.devsuperior.client.repositories.ClientRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository repository;
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Optional<Client> result = repository.findById(id);
+        Client client = result.get();
+        ClientDTO dto = new ClientDTO(client);
+        return dto;
+    }
+}
