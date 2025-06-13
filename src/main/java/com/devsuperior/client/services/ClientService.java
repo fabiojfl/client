15df2d0101a7d51eb49f47bfcc1156717524c35a
Fábio José <fabiojfl@gmail.com>
2025-06-13 08:37:36 -0300

    add method findAll() in class ClientController

--- src/main/java/com/devsuperior/client/services/ClientService.java
+++ src/main/java/com/devsuperior/client/services/ClientService.java
@@ -8,17 +8,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ClientService {
+
     @Autowired
     private ClientRepository repository;
+
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id){
         Optional<Client> result = repository.findById(id);
         Client client = result.get();
         ClientDTO dto = new ClientDTO(client);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClientDTO> findAll(){
+        List<Client> result = repository.findAll();
+        return result.stream().map(x -> new ClientDTO(x)).toList();
     }
 }
